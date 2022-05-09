1a87f75f90cdcf3b07d6e34e3561461294baba14
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-05-09 22:03:47 +0200

    updated deps, sidebar and redeploy to heroku

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,14 @@
+import Sidebar from "./components/navigation/Sidebar";
+
 function App() {
   return (
-    <div>
-      <h1>Meliora Impact</h1>
+    <div className="app-container">
+      <div className="sidebar-container">
+        <Sidebar />
+      </div>
+      <div>
+        <h1>Meliora Impact</h1>
+      </div>
     </div>
   );
 }
---
+++ client/components/navigation/Sidebar.jsx
@@ -0,0 +1,5 @@
+const Sidebar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Sidebar;
