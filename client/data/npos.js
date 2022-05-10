9ab2bbdf3dc62d9ce927f9934ab3a9915c2b92f1
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-10 23:55:11 +0200

    add dummy data for npos

---
+++ client/data/npos.js
@@ -0,0 +1,5 @@
+export const npos = [
+  { id: 1, name: "NPO 1", description: "Info about NPO 1", category: "education" },
+  { id: 2, name: "NPO 2", description: "Info about NPO 2", category: "health" },
+  { id: 3, name: "NPO 3", description: "Info about NPO 3", category: "education" }
+];
