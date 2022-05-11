d730343340f40893ff06149e89269072576a7c4c
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-05-11 17:08:45 +0200

    setting up some routing, and component

--- client/App.jsx
+++ client/App.jsx
@@ -5,16 +5,23 @@ import DiscoverPage from "./components/discover/DiscoverPage";
 import MyNonProfitsPage from "./components/my-non-profits/MyNonProfitsPage";
 import Dashboard from "./components/dashboard/Dashboard";
 import Article from "./components/articles/Article";
+import { LoginPage } from "./components/login/LoginPage";
+import { useState } from "react";
 
 function App() {
+  const [login, setLogin] = useState(true);
   return (
     <div className="app-container">
-      <div className="sidebar-container">
+      {login ?
+        <div> </div>
+        :
+        <div className="sidebar-container">
         <Sidebar />
-      </div>
+      </div>}
       <BrowserRouter>
         <Routes>
           {/* TODO: Insert routes */}
+          <Route path="/login" element={<LoginPage/>} />
           <Route exact path="/" element={<Dashboard />} />
           <Route exact path="/articles" element={<ArticlesPage />} />
           <Route exact path="/articles/article" element={<Article />} />
