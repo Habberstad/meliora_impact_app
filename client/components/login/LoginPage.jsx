321794607116cc4acfac08002c1391f864b039c8
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-05-12 00:46:20 +0200

    almost completed left card, and started form

--- client/components/login/LoginPage.jsx
+++ client/components/login/LoginPage.jsx
@@ -1,14 +1,51 @@
 import MelioraIcon from "../../media/meliora_logo.png";
+import LoginCardImage from "../../media/login_card_img.png";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
 
 export const LoginPage = () => {
   return (
-    <div>
-    <div className="login-left-card">
-      <img src={MelioraIcon} alt="company-icon" />
-
+    <div className="login-page-container">
+      <div className="login-left-card">
+        <img
+          className="login-left-card-icon"
+          src={MelioraIcon}
+          alt="company-icon"
+        />
+        <div className="login-left-card-content">
+          <h1>Meliora Connect</h1>
+          <h1>Together We Can Change The World</h1>
+          <img src={LoginCardImage} alt="company-icon" />
+        </div>
+      </div>
+      <div className="login-container">
+        <div className={"login-content"}>
+          <FormControl>
+            <TextField
+              label="Outlined secondary"
+              color="secondary"
+              focused
+              size="small"
+            />
+            <TextField
+              label="Outlined secondary"
+              color="secondary"
+              focused
+              size="small"
+            />
+            <FormGroup>
+              <FormControlLabel control={<Checkbox />} label="Label" />
+            </FormGroup>
+            <Button variant="contained">Log in</Button>
+          </FormControl>
+        </div>
+      </div>
     </div>
-
-
-    </div>
-  )
-}
+  );
+};
--- client/index.jsx
+++ client/index.jsx
@@ -6,6 +6,7 @@ import ArticlesPage from "./components/articles/ArticlesPage";
 import Article from "./components/articles/Article";
 import DiscoverPage from "./components/discover/DiscoverPage";
 import MyNonProfitsPage from "./components/my-non-profits/MyNonProfitsPage";
+import { LoginPage } from "./components/login/LoginPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,6 +19,7 @@ root.render(
         <Route exact path="/my-non-profits" element={<MyNonProfitsPage />} />
         <Route exact path="/wrapped" element={<MyNonProfitsPage />} />
         <Route exact path="/templates" element={<MyNonProfitsPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
