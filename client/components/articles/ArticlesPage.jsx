2aaafb215f1b7768d69518e67f020d1dd73c5ad2
DennisBrenna <dennis-1@live.se>
2022-05-11 20:57:43 +0200

    backbone

--- client/components/articles/ArticlesPage.jsx
+++ client/components/articles/ArticlesPage.jsx
@@ -1,5 +1,54 @@
+import logo from '../../media/article-header.png';
+import logo2 from '../../media/article-header.png';
 const ArticlesPage = () => {
-  return <h1>Articles Page</h1>;
+  return (
+  <div>
+
+
+
+    <h1>Meliora Articlels</h1>
+
+    <div id={"filter_articles"}>
+        <p>New</p>
+        <p>Popular</p>
+        <p>Water</p>
+        <p>Knowledge</p>
+    </div>
+
+    <div id={"articles-container"}>
+
+    <div id={"test"}>
+      <img src={logo}/>
+    </div>
+    <div id={"test"}>
+      <img src={logo2}/>
+    </div>
+
+
+    </div>
+    <h1>Articles You Should Check Out</h1>
+    <div id={"articles-recommendation-container"}>
+
+      <div id={"test"}>
+        <img src={logo}/>
+      </div>
+
+      <div id={"test"}>
+        <img src={logo2}/>
+      </div>
+
+      <div id={"test"}>
+        <img src={logo2}/>
+      </div>
+
+
+    </div>
+
+
+
+
+  </div>
+);
 };
 
 export default ArticlesPage;
