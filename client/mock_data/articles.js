79df4939320dd16f627e5bf3eba28ac3d99504fb
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-05-12 13:13:16 +0200

    Added mock articles data + initial google sso setup

---
+++ client/mock_data/articles.js
@@ -0,0 +1,18 @@
+export const articles = [
+  {
+    title: "Title not defined yet..",
+    _id: "123j12",
+    description:
+      "We’ve started our work in Burkina Faso. With your help we can provide clean water to several villages",
+    published: "21 December 2021",
+    paragraph_1:
+      "Lorem ipsum dolor sit amet, sed ea solum movet scriptorem, eos dolore evertitur ei, ferri omnium sea at. Harum accusam suscipiantur in pri, constituto appellantur consequuntur ei his. Pro at mollis aliquid civibus. Dicam ornatus ex duo, duo te aeque bonorum. ",
+    paragraph_2:
+      "Lorem ipsum dolor sit amet, sed ea solum movet scriptorem, eos dolore evertitur ei, ferri omnium sea at. Harum accusam suscipiantur in pri, constituto appellantur consequuntur ei his. Pro at mollis aliquid civibus. Dicam ornatus ex duo, duo te aeque bonorum.  Harum accusam suscipiantur in pri, constituto appellantur consequuntur ei his.\n" +
+      "\n" +
+      "Harum accusam suscipiantur in pri, constituto appellantur consequuntur ei his.",
+    paragraph_3:
+      "Lorem ipsum dolor sit amet, sed ea solum movet scriptorem, eos dolore evertitur ei, ferri omnium sea at. Harum accusam suscipiantur in pri, constituto appellantur consequuntur ei his. Pro at mollis aliquid civibus. Dicam ornatus ex duo, duo te aeque bonorum. ",
+    author: "Dennis Brenna",
+  },
+];
