b6e8ed902fd5dcffe77c7b3bcc44fc5fde027ab3
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-05-12 13:37:36 +0200

    almost completed left card, and started form

--- client/components/login/LoginPage.jsx
+++ client/components/login/LoginPage.jsx
@@ -1,13 +1,18 @@
 import MelioraIcon from "../../media/meliora_logo.png";
 import LoginCardImage from "../../media/login_card_img.png";
+import GoogleIcon from "../../media/google_icon.png";
+
 import {
+  Box,
   Button,
   Checkbox,
+  Divider,
   FormControl,
   FormControlLabel,
   FormGroup,
   TextField,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const LoginPage = () => {
   return (
@@ -26,24 +31,60 @@ export const LoginPage = () => {
       </div>
       <div className="login-container">
         <div className={"login-content"}>
-          <FormControl>
-            <TextField
-              label="Outlined secondary"
-              color="secondary"
-              focused
-              size="small"
-            />
-            <TextField
-              label="Outlined secondary"
-              color="secondary"
-              focused
-              size="small"
-            />
-            <FormGroup>
-              <FormControlLabel control={<Checkbox />} label="Label" />
-            </FormGroup>
-            <Button variant="contained">Log in</Button>
-          </FormControl>
+          <div>
+            <div>
+              <h2>login</h2>
+              <p>Log in with:</p>
+            </div>
+            <div>
+              <Link to="/login-google" style={{ textDecoration: "none" }}>
+                <Button
+                  sx={{ mb: "22px" }}
+                  fullWidth
+                  variant={"outlined"}
+                  size={"large"}
+                  onClick={() => {
+                    use;
+                  }}
+                >
+                  <img
+                    style={{ height: "25px" }}
+                    src={GoogleIcon}
+                    alt="GoogleIcon"
+                  />
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <Divider style={{ width: "100%" }}>OR</Divider>
+          <form className="login-form">
+            <FormControl className="login-form">
+              <TextField
+                fullWidth
+                sx={{ mt: "22px" }}
+                label="Email adress"
+                variant="outlined"
+              />
+              <TextField
+                fullWidth
+                sx={{ my: "22px" }}
+                label="Password"
+                variant="outlined"
+              />
+              <FormGroup row>
+                <FormControlLabel control={<Checkbox />} label="Remember me" />
+                <a href={"/"}>Forgot password?</a>
+              </FormGroup>
+              <Button
+                className={"form-button"}
+                sx={{ mt: 1 }}
+                variant="contained"
+                size="large"
+              >
+                Log in
+              </Button>
+            </FormControl>
+          </form>
         </div>
       </div>
     </div>
