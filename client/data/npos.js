0c1b42da658bff0686355f6f8d7b33c0a7a7fb37
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-12 11:57:58 +0200

    add more data

--- client/data/npos.js
+++ client/data/npos.js
@@ -1,5 +1,11 @@
 export const npos = [
-  { id: 1, name: "School 4 Kids", description: "We build schools", category: "education" },
-  { id: 2, name: "The Good Doctors!", description: "Free healthcare for people in x Free healthcare for people in x\"", category: "health" },
-  { id: 3, name: "Train Teachers Intiative", description: "Uganda need teachers to educate the new generation", category: "education" }
+  { id: 1, name: "School 4 Kids", description: "We build schools", category: "knowledge" },
+  { id: 2, name: "The Good Doctors!", description: "Free healthcare for people in x Free healthcare for people in x\"", category: "water" },
+  { id: 3, name: "Train Teachers Intiative", description: "Uganda need teachers to educate the new generation", category: "knowledge" },
+  { id: 4, name: "School 4 Kids", description: "We build schools", category: "water" },
+  { id: 5, name: "The Good Doctors!", description: "Free healthcare for people in x Free healthcare for people in x\"", category: "water" },
+  { id: 6, name: "Train Teachers Intiative", description: "Uganda need teachers to educate the new generation", category: "knowledge" },
+  { id: 7, name: "School 4 Kids", description: "We build schools", category: "knowledge" },
+  { id: 8, name: "The Good Doctors!", description: "Free healthcare for people in x Free healthcare for people in x\"", category: "water" },
+  { id: 9, name: "Train Teachers Intiative", description: "Uganda need teachers to educate the new generation", category: "knowledge" },
 ];
