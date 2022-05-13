47ac9b82379f0e8819f3260e0f8a6d08a2ceedd5
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-13 08:05:02 +0200

    styled search bar

--- client/components/discover/KeywordFilter.jsx
+++ client/components/discover/KeywordFilter.jsx
@@ -1,8 +1,18 @@
+import { InputAdornment, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
 export function KeywordFilter(props) {
+  const placeholderText = "Søk etter organisjoner eller prosjekter"
+
   return (
-    <div>
-      <label>Search: </label>
-      <input type={"text"} onChange={props.onChange} />
+    <div className={"npo-search-field"}>
+      <TextField variant={"outlined"} onChange={props.onChange} size={"small"} InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }} placeholder={placeholderText} fullWidth />
     </div>
   );
 }
