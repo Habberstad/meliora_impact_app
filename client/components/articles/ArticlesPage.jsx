f56bf7f116dd3d38076ed39a2f56ed466993b6ad
DennisBrenna <dennis-1@live.se>
2022-05-15 17:47:03 +0200

    filter css styling

--- client/components/articles/ArticlesPage.jsx
+++ client/components/articles/ArticlesPage.jsx
@@ -5,6 +5,7 @@ import { Grid } from "@mui/material";
 import { useEffect, useState } from "react";
 import "../../styles/articlesPage.css";
 import { getArticles } from "../../api-client/articles";
+import Divider from "@mui/material/Divider";
 
 const ArticlesPage = () => {
   const [articleList, setArticleList] = useState(articles);
@@ -27,12 +28,27 @@ const ArticlesPage = () => {
     <div>
       <h1>Meliora Articles</h1>
 
-      <Grid container direction="row" spacing={1} justifyContent="center">
-        <Grid item>New</Grid>
-        <Grid item>Popular</Grid>
-        <Grid item>Water</Grid>
-        <Grid item>Knowledge</Grid>
-      </Grid>
+      <div className={"filterContainer"}>
+        <Grid container="column" justifyContent={"center"}>
+          <Grid container direction="row" spacing={3} justifyContent="center">
+            <Grid className={"newFilter"} item>
+              New
+            </Grid>
+            <Grid className={"popularFilter"} item>
+              Popular
+            </Grid>
+            <Grid className={"waterFilter"} item>
+              Water
+            </Grid>
+            <Grid className={"knowledgeFilter"} item>
+              Knowledge
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Divider className={"dividerFilter"} />
+          </Grid>
+        </Grid>
+      </div>
 
       <div id={"articles-container"}>
         <Grid container direction={"row"}>
@@ -105,31 +121,60 @@ const ArticlesPage = () => {
       <Grid
         container
         direction="row"
-        justifyContent="flex-start"
+        justifyContent="center"
         alignItems="center"
         spacing={0.5}
       >
         <Grid item xs={5}>
-          <img width={"100%"} height={"100%"} src={articleList[0].image} />
-          <div>{articleList[0].date}</div>
-          <div>{articleList[0].description}</div>
+          <div className={"bigCheckoutContainer"}>
+            <img
+              width={"100%"}
+              height={"100%"}
+              src={articleList[0].image}
+              alt={"adas"}
+            />
+            <div className={"npoTextSmall"}>{articleList[2].npoName}</div>
+            <div className={"bigDateRecommended"}>{articleList[0].date}</div>
+            <div className={"bigDescriptionRecommended"}>
+              {articleList[0].description}
+            </div>
+          </div>
         </Grid>
 
         <Grid item xs={3}>
-          <img width={"100%"} height={"100%"} src={articleList[2].image} />
-          <div>{articleList[2].date}</div>
-          <div>{articleList[2].description}</div>
+          <div className={"smallCheckoutContainer"}>
+            <img
+              width={"100%"}
+              height={"100%"}
+              src={articleList[2].image}
+              alt={"dasd"}
+            />
+            <div className={"npoTextSmall"}>{articleList[2].npoName}</div>
+            <div>
+              <div className={"smallDateRecommended"}>
+                {articleList[2].date}
+              </div>
+            </div>
+            <div className={"smallDescriptionRecommended"}>
+              {articleList[2].description}
+            </div>
+          </div>
         </Grid>
 
         <Grid item xs={3}>
-          <img
-            width={"100%"}
-            height={"100%"}
-            src={articleList[2].image}
-            alt={"das"}
-          />
-          <div>{articleList[2].date}</div>
-          <div>{articleList[2].description}</div>
+          <div className={"smallCheckoutContainer"}>
+            <img
+              width={"100%"}
+              height={"100%"}
+              src={articleList[2].image}
+              alt={"das"}
+            />
+            <div className={"npoTextSmall"}>{articleList[2].npoName}</div>
+            <div className={"smallDateRecommended"}>{articleList[2].date}</div>
+            <div className={"smallDescriptionRecommended"}>
+              {articleList[2].description}
+            </div>
+          </div>
         </Grid>
       </Grid>
     </div>
