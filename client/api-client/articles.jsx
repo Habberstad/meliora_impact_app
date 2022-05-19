13dfa9e381ebca90f5c76917a7fb8de8613b724e
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-19 14:31:42 +0200

    changed link href

---
+++ client/api-client/articles.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { fetchJSON } from "../lib/fetchJSON";
+
+
+
+export const ArticleApiContext = React.createContext({
+
+  async getArticles(query) {
+    return await fetchJSON("/api/articles?"+ new URLSearchParams(query));
+  },
+
+
+});
