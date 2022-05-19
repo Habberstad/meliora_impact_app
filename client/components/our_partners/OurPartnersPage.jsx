f4f601b6e44e857dfc5832ecfb43b51cb1d496d2
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-05-19 19:46:44 +0200

    Partner cards, refactoring and fix css bug in header

--- client/components/our_partners/OurPartnersPage.jsx
+++ client/components/our_partners/OurPartnersPage.jsx
@@ -1,13 +1,44 @@
 import { HeaderPartner } from "./HeaderPartner";
 import { CategoryFilter } from "../discover/CategoryFilter";
 import "../../styles/partners-styles.css";
+import Grid from "@mui/material/Grid";
+import * as PropTypes from "prop-types";
+import { PartnerCard } from "./PartnerCard";
 
+const partners = [
+  {
+    _id: "1",
+    name: "Leve havet",
+    imgage:
+      "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id quisque sed sed eleifend tempor ",
+    category: "Water",
+  },
+  {
+    _id: "2",
+    name: "Good school",
+    imgage:
+      "https://www.uidownload.com/files/893/147/580/big-white-circle-background.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id quisque sed sed eleifend tempor ",
+    category: "Education",
+  },
+];
+
+PartnerCard.propTypes = { partner: PropTypes.any };
 const OurPartnersPage = () => {
   return (
     <div className={"partners-container"}>
       <HeaderPartner />
       <CategoryFilter />
-      <div className={"list-container"}>hei</div>
+      <div className={"list-container"}>
+        <Grid container direction="column" alignItems="stretch">
+          {partners.map((partner) => {
+            return <PartnerCard key={partner._id} partner={partner} />;
+          })}
+        </Grid>
+      </div>
     </div>
   );
 };
