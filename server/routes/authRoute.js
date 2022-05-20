82e7a449b25ab75336002c8c3f095afb334f82aa
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-20 14:27:33 +0200

    fix import path

---
+++ server/routes/authRoute.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+
+import passport from "passport";
+import { config } from "../config/Constants.js";
+
+const router = Router();
+const CLIENT_URL = config.url.API_URL;
+
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+      cookies: req.cookies
+    });
+  }
+});
+
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure"
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed"
+  })
+);
+
+
+export default router;
