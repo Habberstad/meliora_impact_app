b1ef9dd4db3a142d5b9cf23593a40e1a97712327
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-21 11:27:28 +0200

    add accountApiContext

--- server/routes/orgAccountsRoute.js
+++ server/routes/orgAccountsRoute.js
@@ -1,41 +1,57 @@
 import { Router } from "express";
 import { ObjectId } from "mongodb";
-import Account from "../models/orgAccountModel.js"
+import Account from "../models/orgAccountModel.js";
+
+
 
 
 const router = Router();
 
-router.get("/",  async (req, res) => {
+router.get("/", async (req, res) => {
 
-  const { _id }= req.query
-  console.log("test",_id)
-  OrgAccount.findById(_id, function (err, data) {
-    if (err){
-      res.redirect("http://localhost:3000/wrapped")
-    }
-    else{
+  const { _id } = req.query;
+  console.log("test", _id);
+  Account.findById(_id, function(err, data) {
+    if (err) {
+      res.redirect("http://localhost:3000/wrapped");
+    } else {
       res.send(data);
     }
   });
 
-})
+});
 
 
 router.post("/", async (req, res) => {
-  console.log(req.body.org_name)
-  const account = await new Account({
-    org_name: req.query.org_name,
-    org_number: req.query.org_number,
-    google_id: req.query.google_id,
-    subscription: req.query.subscription
-  })
 
-  console.log(account)
+    console.log(req.body)
+    const account =  new Account({ org_name: "halla" });
 
-  res.json(account)
-})
+    await account.save(function(err,saved) {
+      console.log(err)
+      if (err) {
+        console.log("Error: validation failed")
+        res.send({error:"could not save"});
+      } else  {
+        console.log("Saved")
+        res.send(saved);
+      }
+    })
 
 
+});
+
+router.get("/exist", async (req, res) => {
+  const google_id = req.query.value;
+  const account = await Account.find({ google_id });
+
+
+  if (account !== undefined)
+    res.redirect("/logout");
+  else
+    res.redirect("/createAccount");
+
+});
 
 
 export default router;
