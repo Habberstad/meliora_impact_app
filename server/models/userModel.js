c6f323b89ac44ab9ddf1d25b9750c0497f6bd89e
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-23 21:37:53 +0200

    modify user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
   first_name: String,
   last_name: String,
   org_id: String,
+  google_id: String,
   description: String,
+  accessToken: String,
   role: {
     type: String,
     enum: ["USER, ADMIN"],
