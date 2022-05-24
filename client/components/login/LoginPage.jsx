be4f2945b4d4c2450d37b5a3b9292b08d39ad548
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-05-24 12:02:03 +0200

    rafactoring login to implement new flow - clean up loginpage - create simple back button(not finished)

--- client/components/login/LoginPage.jsx
+++ client/components/login/LoginPage.jsx
@@ -1,11 +1,9 @@
 import GoogleIcon from "../../media/google_icon.png";
 import "../../styles/loginPage-styles.css";
 
-import { Button, Divider } from "@mui/material";
 import { Link } from "react-router-dom";
 import { LoginLeftCard } from "./LoginLeftCard";
 import { useEffect, useState } from "react";
-import { FormTermsOfServiceText } from "./FormTermsOfServiceText";
 import { RegisterForm } from "./RegisterForm";
 import { LoginForm } from "./LoginForm";
 
@@ -24,50 +22,7 @@ export const LoginPage = () => {
     <div className="login-page-container">
       {isOverBreakpoint && <LoginLeftCard />}
       <div className="login-container">
-        <div className={"login-content"}>
-          <div>
-            {isRegistered ? (
-              <div>
-                <h2>Login</h2>
-                <p>Log in with:</p>
-              </div>
-            ) : (
-              <div>
-                <h2>Get started for absolutely free</h2>
-                <p>Create account with:</p>
-              </div>
-            )}
-            <div>
-              <div onClick={google} style={{ textDecoration: "none" }}>
-                <Button
-                  sx={{
-                    mb: "22px",
-                    borderColor: "#637381",
-                    "&:hover": {
-                      borderColor: "#000",
-                      backgroundColor: "#FFF",
-                      color: "#637381",
-                    },
-                  }}
-                  fullWidth
-                  variant={"outlined"}
-                  size={"large"}
-                >
-                  <img
-                    style={{ height: "25px" }}
-                    src={GoogleIcon}
-                    alt="GoogleIcon"
-                  />
-                </Button>
-              </div>
-            </div>
-          </div>
-          <Divider style={{ width: "100%" }}>OR</Divider>
-          <form className="login-form">
-            {isRegistered ? <LoginForm /> : <RegisterForm />}
-          </form>
-          <FormTermsOfServiceText />
-        </div>
+        {isRegistered ? <LoginForm google={google} /> : <RegisterForm />}
       </div>
     </div>
   );
