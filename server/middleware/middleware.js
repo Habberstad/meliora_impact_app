4dbdb3a2ccb7afc4ec5c656481b31ac643d783f4
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-24 22:20:40 +0200

    add custom middleware

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,9 +1,7 @@
 import User from "../models/userModel.js";
-import passport from "passport"
+import passport from "passport";
 
- const isLoggedIn = async (req, res, next) => {
-  //const user = await User.find({ google_id: req.user.id });
-
+export const isLoggedIn = async (req, res, next) => {
   if (req.user) {
     next();
   } else {
@@ -11,5 +9,48 @@ import passport from "passport"
   }
 };
 
+export const hasAccount = async (req, res, next) => {
+  const user = await User.find({ google_id: req.user.id });
 
-export default isLoggedIn;
+  if (user.length !== 0 && req.user.id === user[0].google_id) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+export const accessToOwnAccountOnly = async (req, res, next) => {
+  const user = await User.find({ google_id: req.user.id });
+
+  if (user.length !== 0 && req.user.id === user[0].google_id) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+export const roleRequired = (role) => {
+  return async (req, res, next) => {
+    const user = await User.find({ google_id: req.user.id });
+
+    if (user.length !== 0 && user[0].role === role) {
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  };
+};
+
+export const subscriptionTypeRequired = (subscriptionType) => {
+  return async (req, res, next) => {
+    const user = await User.find({ google_id: req.user.id });
+
+    if (user.length !== 0 && user[0].subscription === subscriptionType) {
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  };
+};
+
+export default { isLoggedIn, hasAccount, accessToOwnAccountOnly, roleRequired, subscriptionTypeRequired };
