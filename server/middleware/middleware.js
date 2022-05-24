2dd511b8569adfdb15f1b32d6f9c705e1a3e8496
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-25 00:17:14 +0200

    create custom middleware

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import passport from "passport";
+import { ObjectId } from "mongodb";
 
 export const isLoggedIn = async (req, res, next) => {
   if (req.user) {
@@ -10,6 +11,10 @@ export const isLoggedIn = async (req, res, next) => {
 };
 
 export const hasAccount = async (req, res, next) => {
+  if (!req.user) {
+    return res.sendStatus(401);
+  }
+
   const user = await User.find({ google_id: req.user.id });
 
   if (user.length !== 0 && req.user.id === user[0].google_id) {
@@ -19,18 +24,36 @@ export const hasAccount = async (req, res, next) => {
   }
 };
 
-export const accessToOwnAccountOnly = async (req, res, next) => {
-  const user = await User.find({ google_id: req.user.id });
 
-  if (user.length !== 0 && req.user.id === user[0].google_id) {
-    next();
-  } else {
-    res.sendStatus(401);
+// should change to find (from findById
+export const accessToOwnAccountOnly = async (req, res, next) => {
+  if(!req.user){
+    return res.sendStatus(401);
   }
+
+  const user = await User.find({ google_id : req.user.id });
+
+  try{
+    const id = ObjectId(req.params.id)
+    const id2 = user[0]._id.toString()
+    if (user.length !== 0 && id.equals(id2)) {
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  }catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+
+
 };
 
 export const roleRequired = (role) => {
   return async (req, res, next) => {
+    if(!req.user){
+      return res.sendStatus(401);
+    }
+
     const user = await User.find({ google_id: req.user.id });
 
     if (user.length !== 0 && user[0].role === role) {
@@ -43,6 +66,10 @@ export const roleRequired = (role) => {
 
 export const subscriptionTypeRequired = (subscriptionType) => {
   return async (req, res, next) => {
+    if(!req.user){
+      return res.sendStatus(401);
+    }
+
     const user = await User.find({ google_id: req.user.id });
 
     if (user.length !== 0 && user[0].subscription === subscriptionType) {
@@ -52,5 +79,11 @@ export const subscriptionTypeRequired = (subscriptionType) => {
     }
   };
 };
+
+const extractUrlPathParam = (url) =>{
+  return  url.substring(
+    url.lastIndexOf("/") + 1
+  );
+}
 
 export default { isLoggedIn, hasAccount, accessToOwnAccountOnly, roleRequired, subscriptionTypeRequired };
