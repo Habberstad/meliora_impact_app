8a22138fa1fa112a629e4130ad07bc56a4e62046
kristofferhabberstad <kristoffer.habberstad@gmail.com>
2022-05-25 23:12:53 +0200

    Adding fetch logic + fixed services

--- client/components/non-profit-page/projects-tab/ProjectTabPage.jsx
+++ client/components/non-profit-page/projects-tab/ProjectTabPage.jsx
@@ -8,68 +8,33 @@ import { useState } from "react";
 import { ProjectContent } from "./ProjectContent";
 
 const ProjectTabPage = ({ data }) => {
-  const [selectedTab, setSelectedTab] = useState("project1");
+  const [selectedTab, setSelectedTab] = useState("project4");
 
   const handleNavigationState = (tabValue) => {
     setSelectedTab(tabValue);
   };
 
+  let tabNumber = 0;
+
   return (
     <div className="outer-project-content-container">
       <div className="projects-tab-navigation-section">
-        <Button
-          onClick={() => handleNavigationState("project1")}
-          sx={
-            selectedTab === "project1"
-              ? highlightedOutlinedTabButtonStyle
-              : outlinedTabButtonStyle
-          }
-        >
-          {data.project_1.name}
-        </Button>
-        <Button
-          onClick={() => handleNavigationState("project2")}
-          sx={
-            selectedTab === "project2"
-              ? highlightedOutlinedTabButtonStyle
-              : outlinedTabButtonStyle
-          }
-        >
-          {data.project_2.name}
-        </Button>
-        <Button
-          onClick={() => handleNavigationState("project3")}
-          sx={
-            selectedTab === "project3"
-              ? highlightedOutlinedTabButtonStyle
-              : outlinedTabButtonStyle
-          }
-        >
-          {data.project_3.name}
-        </Button>
-        <Button
-          onClick={() => handleNavigationState("project4")}
-          sx={
-            selectedTab === "project4"
-              ? highlightedOutlinedTabButtonStyle
-              : outlinedTabButtonStyle
-          }
-        >
-          {data.project_4.name}
-        </Button>
+        {data.map((x) => (
+          <Button
+            onClick={() => handleNavigationState(`project${(tabNumber += 1)}`)}
+            sx={
+              selectedTab === `project${(tabNumber += 1)}`
+                ? highlightedOutlinedTabButtonStyle
+                : outlinedTabButtonStyle
+            }
+          >
+            {x.name}
+          </Button>
+        ))}
       </div>
       <div className="project-content-container">
         {selectedTab === "project1" && (
           <ProjectContent data={data.project_1.content} />
-        )}
-        {selectedTab === "project2" && (
-          <ProjectContent data={data.project_2.content} />
-        )}
-        {selectedTab === "project3" && (
-          <ProjectContent data={data.project_3.content} />
-        )}
-        {selectedTab === "project4" && (
-          <ProjectContent data={data.project_4.content} />
         )}
       </div>
     </div>
