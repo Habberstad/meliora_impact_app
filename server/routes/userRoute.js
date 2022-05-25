5c087fec3e6f3edab383935bf885156e12530348
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-26 01:14:08 +0200

    fix url

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/", UserController.list);
 router.get("/:id" , UserController.getById);
 router.get("/google-id/:id" , UserController.getByGoogleId);
-router.post("/register/", UserController.create);
+router.post("/register", UserController.create);
 
 
 export default router;
