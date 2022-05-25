51961bd61ce1335d2c7689451a219ea2b48dd508
kristofferhabberstad <kristoffer.habberstad@gmail.com>
2022-05-25 18:30:05 +0200

    added stepper + logic

--- client/components/media-template/MediaTemplatePage.jsx
+++ client/components/media-template/MediaTemplatePage.jsx
@@ -1,13 +1,53 @@
 import "../../styles/template-styles/template-styles.css";
+import { Box, Button, Step, StepLabel, Stepper } from "@mui/material";
+import { useState } from "react";
+
+const steps = ["Content", "Format", "Customize", "Review"];
 
 const MediaTemplatePage = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = useState({});
+
+  const totalSteps = steps.lenght;
+  const completedSteps = Object.keys(completed).length;
+  const allStepsCompleted = completedSteps === totalSteps;
+
+  const handleNext = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {};
+
   return (
     <div className="template-main-container">
       <div className="template-header-container">test</div>
       <div className="template-content-container">
+        <div className="template-timeline-container">
+          <Box sx={{ width: "500px" }}>
+            <Stepper activeStep={activeStep} alternativeLabel>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Box>
+        </div>
+        <div className="template-content-title">Select Organization</div>
         <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <Button onClick={handleBack} disabled={activeStep === 0}>
+            Back
+          </Button>
+          <Button onClick={handleNext}>Next</Button>
+        </div>
       </div>
     </div>
   );
