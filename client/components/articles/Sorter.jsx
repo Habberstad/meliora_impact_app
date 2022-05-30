9b2016dc3d401a1a6afbc8758bbf3ae8b07cadfe
DennisBrenna <dennis-1@live.se>
2022-05-30 14:19:48 +0200

    endret til all articel

--- client/components/articles/Sorter.jsx
+++ client/components/articles/Sorter.jsx
@@ -1,39 +1,48 @@
-import {Button, Grid} from "@mui/material";
-import {hoverTabStyle, selectedTabStyle} from "../../styles/button-style-config";
+import { Button, Grid } from "@mui/material";
+import {
+  hoverTabStyle,
+  selectedTabStyle,
+} from "../../styles/button-style-config";
 
 export function Sorter(props) {
-    return <div className="articles-sorter">
-        <Grid container justifyContent="center">
-            <Grid className={"all-filter"} item>
-                <Button
-                    style={{textAlign: "center", padding: "20px 30px 5px 30px"}}
-                    className="test-test"
-                    onClick={props.onClick}
-                    sx={props.selectedTab === "" ? selectedTabStyle : hoverTabStyle}
-                >
-                    Test
-                </Button>
-            </Grid>
-            <Grid className={"water-filter"} item>
-                <Button
-                    style={{textAlign: "center", padding: "20px 30px 5px 30px"}}
-                    onClick={props.onClick1}
-                    sx={props.selectedTab === "water" ? selectedTabStyle : hoverTabStyle}
-                >
-                    Water
-                </Button>
-            </Grid>
-            <Grid className={"knowledge-filter"} item>
-                <Button
-                    style={{textAlign: "center", padding: "20px 30px 5px 30px"}}
-                    onClick={props.onClick2}
-                    sx={
-                        props.selectedTab === "knowledge" ? selectedTabStyle : hoverTabStyle
-                    }
-                >
-                    Knowledge
-                </Button>
-            </Grid>
+  return (
+    <div className="articles-sorter">
+      <Grid container justifyContent="center">
+        <Grid className={"all-filter"} item>
+          <Button
+            style={{ textAlign: "center", padding: "20px 30px 5px 30px" }}
+            className="test-test"
+            onClick={props.onClick}
+            sx={props.selectedTab === "" ? selectedTabStyle : hoverTabStyle}
+          >
+            All
+          </Button>
         </Grid>
-    </div>;
+        <Grid className={"water-filter"} item>
+          <Button
+            style={{ textAlign: "center", padding: "20px 30px 5px 30px" }}
+            onClick={props.onClick1}
+            sx={
+              props.selectedTab === "water" ? selectedTabStyle : hoverTabStyle
+            }
+          >
+            Water
+          </Button>
+        </Grid>
+        <Grid className={"knowledge-filter"} item>
+          <Button
+            style={{ textAlign: "center", padding: "20px 30px 5px 30px" }}
+            onClick={props.onClick2}
+            sx={
+              props.selectedTab === "knowledge"
+                ? selectedTabStyle
+                : hoverTabStyle
+            }
+          >
+            Knowledge
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
