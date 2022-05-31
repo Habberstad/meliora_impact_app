a88594b2e8b61856dee657b4d9ffb57df6a9b83f
harryapp <70029200+harryapp@users.noreply.github.com>
2022-05-31 23:13:56 +0200

    finish minimum functionality

--- client/components/financials/Report.jsx
+++ client/components/financials/Report.jsx
@@ -1,82 +1,145 @@
 import * as React from "react";
-import { Box, Grid } from "@mui/material";
+import { Box, Button, Grid, Paper } from "@mui/material";
 import MelioraIcon from "../../media/meliora_logo.png";
 import { DataGrid } from "@mui/x-data-grid";
+import { CurrencyFormater, DateFormater } from "../shared-components/dateFormater";
+import { PieChart } from "react-minimal-pie-chart";
+import { useState } from "react";
 
-const Report = React.forwardRef((props, ref) => {
-  const columns = [
-    { field: "_id", headerName: "Transaction ID", width: 250 },
-    { field: "payment_amount", headerName: "Amount", width: 130 },
-    {
-      field: "date",
-      headerName: "Date",
-      width: 100,
-      type: "date",
-      valueGetter: ({ value }) => new Date(value).toLocaleDateString("no-NO", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric"
-      })
-    },
-    { field: "payment_type", headerName: "Type", type: "number", width: 90 },
-    {
-      field: "test",
-      headerName: "aggregert data",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.type || ""} ${params.row.type || ""}`
-    }
-  ];
-  const user = props.user;
-  const transactions = props.user.donation_history;
-
-
-  const filteredTransactions = transactions.filter(item => new Date(item.date).getFullYear() === props.year);
-
+function DataTableRows(props) {
 
   return (
-    <div ref={ref}>
-      <Grid container className={"report-container"}>
-        <Grid item>
+    <>
+      {props.data.map((item) => (
+        <tr>
+          <td>{item._id}</td>
+          <td><DateFormater date={item.date} /></td>
+          <td><CurrencyFormater numb={item.payment_amount} /></td>
+          <td>{item.payment_frequency}</td>
+          <td>
+            {props.user.npo_partners.map((npo) => {
+              if (npo._id === item.npo_id) return npo.name;
+            })}
+          </td>
+          <td>
+            {props.user.npo_partners.map((npo) => {
+              if (npo._id === item.npo_id) return npo.category;
+            })}
+          </td>
+
+        </tr>
+      ))}
+    </>
+  );
+
+}
+
+const Report = React.forwardRef((props, ref) => {
+  const user = props.user;
+
+  const[year, setYear] = useState(  new Date().getFullYear());
+
+  const transactions = props.user.donation_history;
+  const filteredTransactions = transactions.filter(item => new Date(item.date).getFullYear() === year);
+  const sumAmount = filteredTransactions.reduce( (accumulator, currentValue) => {
+    return accumulator + currentValue.payment_amount;
+  }, 0);
+  function backYear() {
+    setYear(year - 1)
+  }
+
+  function forwardYear() {
+    setYear(year + 1)
+  }
+
+  return (
+    <div ref={ref} className={"donation-history-page-container"}>
+
+      <Grid container>
+        <Grid item xs={6} sx={{
+          display: "none",
+          displayPrint: "block"
+        }}>
+          <img src={MelioraIcon} alt="company-icon" />
+          <h3>Meliora Impact</h3>
+          <br />
+          <h1>Annual Report for {year}</h1>
+          <p>This is an overview of your contributions during the period 01.01.{year} - 31.12.{year}</p>
+          <br />
+          <br />
+          <br />
+        </Grid>
+
+        <Grid item xs={6}>
           <Box sx={{
             display: "none",
-            displayPrint: "block",
+            displayPrint: "block"
           }}>
-            <img src={MelioraIcon} alt="company-icon" />
-            <br />
-            <h1>Annual Report for {props.year}</h1>
-            <br />
-            <p>To</p>
-            <h3>{user.org_name}</h3>
-            <h3>{user.name}</h3>
-            <br />
-            <br />
+            <h3>Your Company</h3>
+            <p>org. number {user.org_number}</p>
+            <p>{user.org_name}</p>
+            <p>Storgaten 0334</p>
+            <p>0304 Oslo</p>
           </Box>
+        </Grid>
+        {/* **************** OUTSIDE IS VISIBLE IN ON PRINT OUT  **********************************************************************************/}
+
+
+        <Grid item xs={12} >
+          <Box sx={{
+            display: "block",
+            displayPrint: "none"
+          }}>
+            <Button onClick={backYear} >back</Button>
+            {year}
+            <Button onClick={forwardYear}>forward</Button>
+          </Box >
+
+
+            <table className={"styled-table"}>
+              <thead>
+              <tr>
+                <th>Transactions ID</th>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Type</th>
+                <th>Organization</th>
+                <th>Category</th>
+              </tr>
+              </thead>
+              <tbody>
+              <DataTableRows data={filteredTransactions} user={user} />
+              </tbody>
+              <tfoot>
+              <tr>
+                <td></td>
+                <td>TOTAL</td>
+                <td>
+                  <CurrencyFormater numb={sumAmount} />
+                </td>
+              </tr>
+              </tfoot>
+            </table>
+
+
+          </Grid>
+
+
+        <Grid item xs={6}>
+          <div>
+            *You are entitled to a tax deduction for your contribution.
+            We will ensure that the contribution is reported to the tax authorities.
+            More informasjon can be found at www.skatteetaten.no.
+          </div>
         </Grid>
 
 
-        <Grid item>
-          <Box >
-            <div style={{ height: 500, width: 800 }}>
-              <DataGrid
-                rows={filteredTransactions}
-                columns={columns}
-                getRowId={(row) => row._id}
-              />
-            </div>
-            <div>
-              Total: {filteredTransactions.reduce ((accum, item) => {return accum + item.payment_amount}, 0)}
-            </div>
-
-          </Box>
-        </Grid>
-
+        {/* **************** OUTSIDE IS ONLY VISIBLE ON PRINT OUT **********************************************************************************/}
         <Grid item sx={{
-            display: "none",
-            displayPrint: "block"}}>
-          Bottom
+          display: "none",
+          displayPrint: "block"
+        }}>
+
         </Grid>
       </Grid>
     </div>
--- client/components/shared-components/dateFormater.jsx
+++ client/components/shared-components/dateFormater.jsx
@@ -1,14 +1,22 @@
 export function DateFormater(props) {
   const date = new Date(props.date)
-  const month = date.toLocaleString("no-NO", {month: "long"});
+  const month = date.toLocaleString("no-NO", {month: "2-digit"});
   const day = date.toLocaleString("no-NO", {day: "2-digit"});
-  console.log(date)
+
   const year = date.getFullYear();
 
   return (
     <div>
-      {day + " " +month + " " + year}
+      {day +month  + year}
     </div>
     );
 
 }
+
+
+
+export function CurrencyFormater(props){
+  return new Intl.NumberFormat("no-NO", { style: 'currency',   maximumFractionDigits: 0,
+    minimumFractionDigits: 0,  currency: "NOK" }).format(props.numb)
+}
+
