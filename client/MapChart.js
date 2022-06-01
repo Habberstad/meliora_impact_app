ed3746910bde582a5dceabbffc58a1909d1c2d6f
kristofferhabberstad <kristoffer.habberstad@gmail.com>
2022-06-01 23:22:28 +0200

    Refactor top tiles in project

--- client/MapChart.js
+++ client/MapChart.js
@@ -9,20 +9,6 @@ import {
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-/*const markers = [
-  {
-    markerOffset: -15,
-    name: "Buenos Aires",
-    coordinates: [-58.3816, -34.6037],
-  },
-  { markerOffset: -15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-  {
-    markerOffset: -15,
-    name: "Oslo",
-    coordinates: [10.85862177395285, 59.90822071774486],
-  },
-];*/
-
 const MapChart = ({ markers }) => {
   return (
     <ComposableMap>
--- client/components/non-profit-page/impact-tab/ImpactTabContent.jsx
+++ client/components/non-profit-page/impact-tab/ImpactTabContent.jsx
@@ -15,15 +15,16 @@ import PinkMiddleTile from "./PinkMiddleTile";
 import BlueMiddleTile from "./BlueMiddleTile";
 import GreenMiddleTile from "./GreenMiddleTile";
 
-const ImpactTabContent = ({ data }) => {
+const ImpactTabContent = ({ data, category }) => {
   console.log(data);
+  console.log(category);
   return (
     <div className="impact-main-container">
       <div className="impact-top-tile-section">
-        <GreenImpactTile data={data.top_tile_1} />
-        <BlueImpactTile data={data.top_tile_2} />
-        <PinkImpactTile data={data.top_tile_3} />
-        <GreenImpactTile data={data.top_tile_4} />
+        <GreenImpactTile category={category} data={data.top_tile_1} />
+        <BlueImpactTile category={category} data={data.top_tile_2} />
+        <PinkImpactTile category={category} data={data.top_tile_3} />
+        <GreenImpactTile category={category} data={data.top_tile_4} />
       </div>
       <div className="impact-media-section">
         <LeftMediaCard />
@@ -31,10 +32,10 @@ const ImpactTabContent = ({ data }) => {
       </div>
       <div className="impact-data-section">
         <div className="left-data-container">
-          <YellowMiddleTile data={data.middle_tile_1} />
-          <PinkMiddleTile data={data.middle_tile_2} />
-          <BlueMiddleTile data={data.middle_tile_3} />
-          <GreenMiddleTile data={data.middle_tile_4} />
+          <YellowMiddleTile category={category} data={data.middle_tile_1} />
+          <PinkMiddleTile category={category} data={data.middle_tile_2} />
+          <BlueMiddleTile category={category} data={data.middle_tile_3} />
+          <GreenMiddleTile category={category} data={data.middle_tile_4} />
         </div>
         <DataSectionMediaCard />
       </div>
