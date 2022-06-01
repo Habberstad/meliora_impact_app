01361a02b7d4a78a07288c6d560bca1031108ed1
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-06-01 19:41:27 +0200

    top nav bar

--- client/components/navigation/TopNavBar.jsx
+++ client/components/navigation/TopNavBar.jsx
@@ -4,40 +4,87 @@ import DarkModeIconMoon from "../../media/moonDarkmodeIcon.png";
 import NotificationIconBell from "../../media/bellIcon.png";
 import ProfilePicturePlaceholder from "../../media/profilePicturePlaceholder.png";
 import { IconButton } from "@mui/material";
+import { useState } from "react";
+import * as PropTypes from "prop-types";
+import { NavbarDropDown } from "./NavbarDropDown";
+
+NavbarDropDown.propTypes = {
+  anchorEl: PropTypes.any,
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onClick: PropTypes.func,
+};
 
 export const TopNavBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleProfileClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogoutClick = () => {
+    window.location.href = window.location.origin + "/auth/logout";
+  };
+
   return (
     <div className={"top-navbar-container"}>
       <div className={"top-navbar-icon-container"}>
-        <IconButton aria-label="Select language">
-          <img
-            src={LanguageIconNorway}
-            alt="LanguageIconNorway"
-            className={"top-navbar-icon-button"}
+        <div className={"top-navbar-icon-button-container"}>
+          <IconButton aria-label="Select language">
+            <img
+              src={LanguageIconNorway}
+              alt="LanguageIconNorway"
+              className={"top-navbar-icon-button"}
+            />
+          </IconButton>
+        </div>
+        <div className={"top-navbar-icon-button-container"}>
+          <IconButton aria-label="Darkmode">
+            <img
+              src={DarkModeIconMoon}
+              alt="Darkmode icon"
+              className={"top-navbar-icon-button"}
+            />
+          </IconButton>
+        </div>
+
+        <div className={"top-navbar-icon-button-container"}>
+          <IconButton aria-label="notifications">
+            <img
+              src={NotificationIconBell}
+              alt="Notification icon"
+              className={"top-navbar-icon-button"}
+            />
+          </IconButton>
+        </div>
+
+        <div className={"top-navbar-icon-button-container"}>
+          <IconButton
+            aria-label="profile"
+            aria-controls={open ? "account-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleProfileClick}
+          >
+            <img
+              src={ProfilePicturePlaceholder}
+              alt="Notification icon"
+              className={"top-navbar-icon-button"}
+              style={{ width: "35px" }}
+            />
+          </IconButton>
+          <NavbarDropDown
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            onClick={handleLogoutClick}
           />
-        </IconButton>
-        <IconButton aria-label="Darkmode">
-          <img
-            src={DarkModeIconMoon}
-            alt="Darkmode icon"
-            className={"top-navbar-icon-button"}
-          />
-        </IconButton>
-        <IconButton aria-label="notifications">
-          <img
-            src={NotificationIconBell}
-            alt="Notification icon"
-            className={"top-navbar-icon-button"}
-          />
-        </IconButton>
-        <IconButton aria-label="profile">
-          <img
-            src={ProfilePicturePlaceholder}
-            alt="Notification icon"
-            className={"top-navbar-icon-button"}
-            style={{ width: "30px" }}
-          />
-        </IconButton>
+        </div>
       </div>
     </div>
   );
