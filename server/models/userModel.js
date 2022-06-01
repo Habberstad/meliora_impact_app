7ab2c950c6b12bcb8ac368e38c375a7e4d70a746
harryapp <70029200+harryapp@users.noreply.github.com>
2022-06-01 10:46:50 +0200

    user model: add new fields

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,42 +4,62 @@ import { ObjectId } from "mongodb";
 const userSchema = new mongoose.Schema({
   name: String,
   last_name: String,
+
   email: {
     type: String,
     unique: true
   },
+
   org_number: {
     type: String,
     required: true,
     unique: true
   },
+
   org_name: {
     type: String,
     required: true
+
   },
   google_id: {
     type: String,
     required: true,
     unique: true
   },
-  address: String,
-  postal_code: String,
-  city: String,
+
+  address: {
+    type: String,
+    required: true,
+  },
+
+  postal_code: {
+    type: String,
+    required: true,
+  },
+
+  city: {
+    type: String,
+    required: true,
+  },
+
   payment_option: {
     type: String,
     enum: ["vipps", "klarna"],
     required: true
   },
+
   subscription_type: {
     type: String,
     enum: ["freemium", "premium"],
     required: true
   },
+
   role: {
     type: String,
     enum: ["USER", "ADMIN"],
     default: "USER"
   }
+
 }, {
   collection: "users",
   autoCreate: false
