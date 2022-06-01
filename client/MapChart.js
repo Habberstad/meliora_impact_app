42c17cff17d49655ba90f2cbc2afa47561de1a68
kristofferhabberstad <kristoffer.habberstad@gmail.com>
2022-06-01 13:03:56 +0200

    Updated donation layout

--- client/MapChart.js
+++ client/MapChart.js
@@ -16,39 +16,32 @@ const markers = [
     coordinates: [-58.3816, -34.6037],
   },
   { markerOffset: -15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-  { markerOffset: 25, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
-  { markerOffset: 25, name: "Santiago", coordinates: [-70.6693, -33.4489] },
-  { markerOffset: 25, name: "Bogota", coordinates: [-74.0721, 4.711] },
-  { markerOffset: 25, name: "Quito", coordinates: [-78.4678, -0.1807] },
-  { markerOffset: -15, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
-  { markerOffset: -15, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
-  { markerOffset: 25, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
-  { markerOffset: 25, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
-  { markerOffset: -15, name: "Caracas", coordinates: [-66.9036, 10.4806] },
-  { markerOffset: -15, name: "Lima", coordinates: [-77.0428, -12.0464] },
+  {
+    markerOffset: -15,
+    name: "Oslo",
+    coordinates: [10.85862177395285, 59.90822071774486],
+  },
 ];
 
 const MapChart = () => {
   return (
     <ComposableMap
-      projection="geoAzimuthalEqualArea"
+      projection="geoEqualEarth"
       projectionConfig={{
-        rotate: [58, 20, 0],
-        scale: 400,
+        rotate: [0, 0, 0],
+        scale: 100,
       }}
     >
       <Geographies geography={geoUrl}>
         {({ geographies }) =>
-          geographies
-            .filter((d) => d.properties.REGION_UN === "Americas")
-            .map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill="#EAEAEC"
-                stroke="#D6D6DA"
-              />
-            ))
+          geographies.map((geo) => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              fill="#EAEAEC"
+              stroke="#D6D6DA"
+            />
+          ))
         }
       </Geographies>
       {markers.map(({ name, coordinates, markerOffset }) => (
