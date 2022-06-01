5482b80570f415372e6f9baecd8a4ad521bd9dff
harryapp <70029200+harryapp@users.noreply.github.com>
2022-06-01 23:42:38 +0200

    add empty page for billing

---
+++ client/components/financials/BillingPage.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+export const BillingPage = () => {
+
+  return (
+    <div>
+      <h1>Billing</h1>
+
+      <div>
+        <h3>Payment method</h3>
+      </div>
+
+      <div>
+        <h3>Subscription type</h3>
+      </div>
+
+
+    </div>
+  )
+
+}
