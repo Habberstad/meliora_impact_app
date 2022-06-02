232b7fc2d2a9985dc8c0a8a1d45514164e1c44eb
kristofferhabberstad <kristoffer.habberstad@gmail.com>
2022-06-02 19:28:25 +0200

    Added map to npo

---
+++ client/HarryMapChart.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const markers = [
+  {
+    markerOffset: -15,
+    name: "Buenos Aires",
+    coordinates: [-58.3816, -34.6037],
+  },
+  { markerOffset: -15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
+  { markerOffset: 25, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+  { markerOffset: 25, name: "Santiago", coordinates: [-70.6693, -33.4489] },
+  { markerOffset: 25, name: "Bogota", coordinates: [-74.0721, 4.711] },
+  { markerOffset: 25, name: "Quito", coordinates: [-78.4678, -0.1807] },
+];
+
+const HarryMapChart = () => {
+  return (
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [58, 20, 0],
+        scale: 400,
+      }}
+    >
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies
+            .filter((d) => d.properties.REGION_UN === "Americas")
+            .map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill="#EAEAEC"
+                stroke="#D6D6DA"
+              />
+            ))
+        }
+      </Geographies>
+      {markers.map(({ name, coordinates, markerOffset }) => (
+        <Marker key={name} coordinates={coordinates}>
+          <circle r={10} fill="#A400FF" stroke="#A400FF" strokeWidth={2} />
+          <text
+            textAnchor="middle"
+            y={markerOffset}
+            style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+          >
+            {name}
+          </text>
+        </Marker>
+      ))}
+    </ComposableMap>
+  );
+};
+
+export default HarryMapChart;
