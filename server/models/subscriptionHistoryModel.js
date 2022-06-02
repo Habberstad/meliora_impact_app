3d0eb3172e4ee45be1ea4e2628b5fbb4080df91a
harryapp <70029200+harryapp@users.noreply.github.com>
2022-06-02 13:51:37 +0200

    add subscriptionHistoryModel.js

---
+++ server/models/subscriptionHistoryModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import { ObjectId } from "mongodb";
+
+const subscriptionHistorySchema = new mongoose.Schema({
+  npo_id: {
+    type: ObjectId,
+    required: true
+  },
+
+  user_id: {
+    type: String,
+    required: true
+  },
+
+  payment_amount: {
+    type: Number,
+    required: true
+  },
+
+  payment_frequency: {
+    type: String,
+    required: true
+  },
+
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+
+});
+
+export default mongoose.model("SubscriptionHistory", subscriptionHistorySchema, "subscriptionsHistory");
