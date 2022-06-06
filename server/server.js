4f4fd5651fbcf4fe8da9d54a1d7ac0e812b2395c
harryapp <70029200+harryapp@users.noreply.github.com>
2022-06-06 13:01:05 +0200

    refactor server: modularized server

--- server/server.js
+++ server/server.js
@@ -7,62 +7,57 @@ import passport from "passport";
 import cookieSession from "cookie-session";
 import cors from "cors";
 import mongoose from "mongoose";
-import projectsRoute from "./routes/projectsRoute.js";
-import articlesRoute from "./routes/articlesRoute.js";
 import { config } from "./config/Constants.js";
-import npoRoute from "./routes/npoRoute.js";
-import userRoute from "./routes/userRoute.js";
 import { isAuthenticated, hasAccount, accessToOwnAccountOnly } from "./middleware/middleware.js";
 import passportSetup from "./middleware/passport.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import subscriptionRoutes from "./routes/subscriptionRoutes.js";
+import npoRoute from "./routes/npoRoute.js";
+import userRoute from "./routes/userRoute.js";
+import projectsRoute from "./routes/projectsRoute.js";
+import articlesRoute from "./routes/articlesRoute.js";
 
-const app = express();
 dotenv.config();
 
-app.use(bodyParser.json());
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(express.static("../client/dist"));
+export const initServer = () => {
+  const app = express();
 
-app.use(
-  cookieSession({ name: "session", keys: [process.env.COOKIE_SECRET], maxAge: 24 * 60 * 60 * 1000})
-);
+  app.use(bodyParser.json());
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(express.static("../client/dist"));
+  app.use(
+    cookieSession({ name: "session", keys: [process.env.COOKIE_SECRET], maxAge: 24 * 60 * 60 * 1000 })
+  );
 
-await mongoose.connect(
-  process.env.MONGODB_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-app.use(passport.initialize());
-app.use(passport.session());
+  app.use(
+    cors({
+      origin: config.url.API_URL,
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true
+    })
+  );
 
-app.use(
-  cors({
-    origin: config.url.API_URL,
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
-  })
-);
+  app.use("/auth", authRoutes);
+  app.use("/api/projects", hasAccount, projectsRoute);
+  app.use("/api/articles", hasAccount, articlesRoute);
+  app.use("/api/npo", npoRoute);
+  app.use("/api/subscriptions", hasAccount, subscriptionRoutes);
+  app.use("/api/users", userRoute);
 
-app.use("/auth", authRoutes);
-app.use("/api/projects", hasAccount, projectsRoute);
-app.use("/api/articles", hasAccount, articlesRoute);
-app.use("/api/npo", hasAccount, npoRoute);
-app.use("/api/subscriptions", hasAccount, subscriptionRoutes)
-app.use("/api/users", userRoute);
+  app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+      res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+      res.redirect("/");
+    }
+  });
+
+  return app;
+};
 
 
-app.use((req, res, next) => {
-  if (req.method === "GET" && !req.path.startsWith("/api")) {
-    res.sendFile(path.resolve("../client/dist/index.html"));
-  } else {
-    res.redirect("/");
-  }
-});
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Started on http://localhost:${server.address().port}`);
-});
