7a3d0490c6a3bdcd757b4d36868a9d4f996f1238
Skjalg Hovland Holtbakk <skjalg.hh@hotmail.com>
2022-06-07 12:59:14 +0200

    Implement button B-4.3 in buttton style config

--- client/styles/button-style-config.js
+++ client/styles/button-style-config.js
@@ -46,10 +46,8 @@ export const ReadMoreButtonB31 = {
   },
 };
 
-//---------------------
-//styleguide byttons end
-
-export const navButtonStyle = {
+//B-4.3
+export const filterButtonB43 = {
   fontFamily: "Montserrat",
   boxShadow:
     "0px 0px 2px rgba(148, 157, 176, 0.24), 0px 16px 32px -4px rgba(148, 157, 176, 0.24)",
@@ -68,25 +66,12 @@ export const navButtonStyle = {
       "0px 0px 2px rgba(148, 157, 176, 0.24), 0px 16px 32px -4px rgba(148, 157, 176, 0.24)",
   },
 };
-export const highlightedNavButtonStyle = {
-  fontFamily: "Montserrat",
-  boxShadow:
-    "0px 0px 2px rgba(148, 157, 176, 0.24), 0px 16px 32px -4px rgba(148, 157, 176, 0.24)",
-
-  width: "190px",
-  height: "60px",
-  borderRadius: "8px",
+export const highlightedFilterButtonB43 = {
   backgroundColor: "#B8F0DA",
-  textTransform: "none",
-  fontSize: "16px",
-  fontWeight: 500,
-  color: "#353535",
-  "&:hover": {
-    backgroundColor: "#B8F0DA",
-    boxShadow:
-      "0px 0px 2px rgba(148, 157, 176, 0.24), 0px 16px 32px -4px rgba(148, 157, 176, 0.24)",
-  },
 };
+
+//---------------------
+//styleguide byttons end
 
 export const outlinedTabButtonStyle = {
   color: "#000",
