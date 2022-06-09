00779104408ab314129bf32a4f63bdda68f028e7
harryapp <70029200+harryapp@users.noreply.github.com>
2022-06-09 11:21:20 +0200

    Transaction API: add create route and controller

--- client/api-client/transactionApiContext.jsx
+++ client/api-client/transactionApiContext.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { fetchJSON } from "../lib/fetchJSON";
+import { postJSON } from "../lib/postJSON";
 
 
 export const TransactionApiContext = React.createContext({
 
   async getCurrentUsersTransactions(query) {
     return await fetchJSON("/api/transactions/user/logged-in?" + new URLSearchParams(query));
+  },
+
+  async registerSubscription(subscription) {
+    return await postJSON("/api/transactions", subscription);
   },
 
 });
